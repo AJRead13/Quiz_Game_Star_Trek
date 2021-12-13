6b12a0a392aae3fddcc46b2f3f593acca1c1979f
Andrew Read <aj.read13@gmail.com>
2021-12-12 20:39:26 -0500

    added some js elements, css, and html

---
+++ assets/script/script.js
@@ -0,0 +1,26 @@
+var quiz
+var questionOne
+var questionTwo
+var questionThree
+var questionFour
+var questionFive
+var userScore
+var timer = 
+var userName
+
+var startgame=[]
+
+var timeInterval = setInterval(function () {
+    if (timeLeft > 1) {
+      timerEl.textContent = timeLeft + ' seconds remaining';
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      timerEl.textContent = timeLeft + ' second remaining';
+      timeLeft--;
+    } else {
+      timerEl.textContent = '';
+      clearInterval(timeInterval);
+      displayMessage();
+    }
+  }, 1000);
+}
